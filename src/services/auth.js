4047f7e9aba924786ef293ba214327b451377d3b
MykhailoVobolis <mvobolis@gmail.com>
2024-07-03 19:08:43 +0300

    add hashing password

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,14 @@
+import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 
+export const findUser = (filter) => UsersCollection.findOne(filter);
+
 export const registerUser = async (payload) => {
-  // Перевірка що користувач із такою поштою ще не існує в системі
+  // Хешування паролів за допомогою бібліотеки bcrypt
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+
+  // Валідацію унікальності. Перевірка, чи існує користувач з таким email
   const userEmail = await UsersCollection.findOne({
     email: payload.email,
   });
@@ -11,5 +17,8 @@ export const registerUser = async (payload) => {
     throw createHttpError(409, 'Email in use');
   }
 
-  return await UsersCollection.create(payload);
+  return await UsersCollection.create({
+    ...payload,
+    password: encryptedPassword,
+  });
 };
